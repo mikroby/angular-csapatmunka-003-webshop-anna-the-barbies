f4cc3e4929c8397a7984be6032940b2f97a25487
kucseranna <88683177+kucseranna@users.noreply.github.com>
2022-01-22 11:52:48 +0100

    constructors updated

--- src/app/model/category.ts
+++ src/app/model/category.ts
@@ -1,12 +1,14 @@
 export class Category {
+  [key:string]: any;
   id: number = 0;
   name: string = '';
   description: string = '';
 
-  constructor(parameters: Category) {
-    this.id = parameters.id;
-    this.name = parameters.name;
-    this.description = parameters.description;
+  constructor(parameters?: Category) {
+    if(!parameters) {return}
+    for (const key of Object.keys(parameters)){
+      this[key] = parameters[key];
+    }
   }
 }
 
