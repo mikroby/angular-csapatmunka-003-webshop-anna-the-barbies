3c728c1380128020832d3e11333c459b9c11ab7a
mikroby <89554907+mikroby@users.noreply.github.com>
2022-01-24 15:04:45 +0100

    Revert "Tamás"

--- src/app/common/product-slider/product-slider.component.ts
+++ src/app/common/product-slider/product-slider.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Product } from './../../model/product';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-slider',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-slider.component.scss']
 })
 export class ProductSliderComponent implements OnInit {
+
+  @Input() list: Product[] = [new Product()];
 
   constructor() { }
 
