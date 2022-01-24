de2b3dc78fefcdb50c3b452781689070f7ad188a
mikroby <rtamas@hotmail.com>
2022-01-24 02:12:28 +0100

    home compoent test phase

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/model/product';
 import { ProductService } from 'src/app/service/product.service';
+
 
 @Component({
   selector: 'app-home',
@@ -8,11 +10,27 @@ import { ProductService } from 'src/app/service/product.service';
 })
 export class HomeComponent implements OnInit {
 
+  allFeatured: Product[] = this.productService.getFeaturedAll();
+  featuredToDisplay: Product[] = this.getRandom(this.allFeatured);
+  allProduct: Product[] = this.productService.getAll();
+  discountToDisplay: Product[] = this.getRandom(this.allProduct);
+  
   constructor(
     private productService: ProductService,
   ) { }
 
   ngOnInit(): void {
+  }
+
+  getRandom(list: Product[], num: number = 5): Product[] {
+    const copyList = [...list];
+    const result = [];
+    for (let i = 0; i < num; i++) {
+      const index = Math.floor(Math.random() * list.length);
+      result.push(copyList[index]);
+      copyList.splice(index, 1);
+    }
+    return result;
   }
 
 }
