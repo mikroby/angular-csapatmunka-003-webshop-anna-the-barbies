f1869d11055fe65cc2492bde0f6a77493d20ddfa
zsofip <91950899+zsofip@users.noreply.github.com>
2022-01-28 19:11:31 +0100

    service even more corrected

--- src/app/common/product-card/product-card.component.ts
+++ src/app/common/product-card/product-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, switchMap } from 'rxjs';
 import { Product } from 'src/app/model/product';
+import { ProductService } from 'src/app/service/product.service';
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
@@ -7,11 +10,17 @@ import { Product } from 'src/app/model/product';
 })
 
 export class ProductCardComponent implements OnInit {
-  @Input() currentProduct: Product = new Product();
+  // @Input() currentProduct: Product = new Product();
+  currentProduct$: Observable<Product> = this.ar.params.pipe(
+    switchMap( params => this.productService.getOne(params['id'])),
+  );
 
   src: string = "";
 
-  constructor() { }
+  constructor(
+    private ar: ActivatedRoute,
+    private productService: ProductService
+    ) { }
 
   ngOnInit(): void {
     this.src=`/assets/img/${this.currentProduct.image}`;
